b771cda17949c8b28c954da3152641f37cc5d627
Zack Zhou <i@lotc.cc>
2019-01-12 14:06:23 +0800

    Folder stucture for a clean new look

--- wfstate.py
+++ wfstate.py
@@ -43,7 +43,7 @@ data_files = {
 data_dict = {}
 
 for k in data_files:
-	with open(os.path.dirname(os.path.abspath(__file__)) + k, 'r', encoding='utf-8') as E:
+	with open(os.path.dirname(os.path.abspath(__file__)) + '\\data\\' + k, 'r', encoding='utf-8') as E:
 		data_dict[data_files[k]] = json.loads(E.read())
 
 # Riven data
