7fe830d032567350649a64a9dc47601ab2d114e0
Raka-loah <i@lotc.cc>
2020-03-31 09:51:53 +0800

    Misc: turnip?

--- misc.py
+++ misc.py
@@ -903,3 +903,31 @@ def msg_exchange_rate_2(j):
             return ''
     msg = '命令格式：/汇率 数字 货币种类，支持小数点，货币种类如USD EUR HKD等。'
     return msg
+
+def ac_turnip(j):
+    msg = ''
+    match = re.match(r'.* (\d+) (\d+)', j['message'])
+    if match:
+        try:
+            if 90 <= int(match.group(1)) <= 110:
+                rate = float(match.group(2)) / float(match.group(1))
+                wave = f'\n【波动型】最大值{int(match.group(1))*0.9:.0f}~{int(match.group(1))*1.4:.0f}。'
+                three = f'\n【3期型】最大值{int(match.group(1))*2.0:.0f}~{int(match.group(1))*6.0:.0f}。'
+                four = f'\n【4期型】最大值{int(match.group(1))*1.4:.0f}~{int(match.group(1))*2.0:.0f}。'
+                if rate >= 0.91:
+                    msg = f'比例为{rate * 100:.2f}%，预测为【波动型】或【4期型】。{wave}{four}'
+                elif rate >= 0.85:
+                    msg = f'比例为{rate * 100:.2f}%，预测为【递减型】或【3期型】或【4期型】。{three}{four}'
+                elif rate >= 0.80:
+                    msg = f'比例为{rate * 100:.2f}%，预测为【3期型】或【4期型】。{three}{four}'
+                elif rate >= 0.60:
+                    msg = f'比例为{rate * 100:.2f}%，预测为【波动型】或【4期型】。{wave}{four}'
+                else:
+                    msg = f'比例为{rate * 100:.2f}%，预测为【4期型】。{four}'
+            else:
+                msg = f'错误：购入价格必须位于90~110区间。'
+        except:
+            pass
+    else:
+        msg = f'命令格式：{j["keyword"]} 购入价格 周一上午价格'
+    return msg
