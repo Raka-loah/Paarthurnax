9b2c6624507378fb607fd375f67069618e2575af
shirleymatrix <731114464@qq.com>
2021-03-23 08:25:59 +0000

    feat: import vue3

--- Paarthurnax/admin/js/main.js
+++ Paarthurnax/admin/js/main.js
@@ -194,4 +194,19 @@ $(document).ready(function(){
       
     });
 
+    /* VUE CODE */
+    const app = Vue.createApp({})
+    app.component('button-counter', {
+      data() {
+        return {
+          count: 0
+        }
+      },
+      template: `
+        <button @click="count++">
+          You clicked me {{ count }} times.
+        </button>`
+    })
+
+    app.mount('#app')
 });
